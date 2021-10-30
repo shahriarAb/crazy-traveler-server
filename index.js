532d44a1944537ad63209e6d3c9aca6808e3fcf0
shahriarAb <sm.abeed007@gmail.com>
2021-10-31 01:58:40 +0600

    first commit and first api added

---
+++ index.js
@@ -0,0 +1,52 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const ObjectId = require('mongodb').ObjectId;
+
+const port = process.env.PORT || 5500;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8cjcg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("travelInfo");
+        const placeCollection = database.collection("destinations");
+
+        //GET api
+        app.get('/destinations', async (req, res) => {
+            const cursor = placeCollection.find({});
+            const destinations = await cursor.toArray();
+            res.send(destinations);
+        })
+
+        //GET api to get specific item
+        app.get('/destination/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const destination = await placeCollection.findOne(query);
+            res.send(destination);
+        })
+
+    }
+    finally {
+        // await client.close();
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Crazy Traveler Server is running.')
+});
+
+app.listen(port, () => {
+    console.log(`Server is now running on http://localhost:${port}`);
+});
